5ee6c88f1fa8abfb46f1b733316323ba04989866
Mariusz Nowostawski <mariusz@nowostawski.org>
2012-06-17 00:44:28 +1200

    Some missing files.

---
+++ server/public/js/socials.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+
+  (function() {
+      // Google +1 button
+      // place this 
+      // where you want the button to render
+      // <g:plusone annotation="inline"></g:plusone>
+      
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+
+</script>
